3b089c9568621e3c3c6c85a54b4821b442b706f0
vsspnkkvr <112337917+vsspnkkvr@users.noreply.github.com>
2023-04-29 20:03:36 -0400

    Has Many Through Associations Magazine Models

---
+++ app/models/subscriber.rb
@@ -0,0 +1,4 @@
+class Subscriber < ApplicationRecord
+    has_many :subscriptions
+    has_many :magazines, through: :subscriptions  
+end
---
+++ db/migrate/20230429235744_create_magazines.rb
@@ -0,0 +1,9 @@
+class CreateMagazines < ActiveRecord::Migration[7.0]
+  def change
+    create_table :magazines do |t|
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
